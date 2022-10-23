15d0dbe9e3e78b901d407b9b4abccc7e7fe2678b
Immanuella Busari <immanuella.busari@valtech.com>
2022-10-23 09:14:12 +0100

    Fix percentage

--- web-app/src/pages/Dashboard.tsx
+++ web-app/src/pages/Dashboard.tsx
@@ -9,13 +9,13 @@ import { CarbonChart } from '../components/Dashboard/CarbonChart';
 
 const Dashboard = () => {
     // Calculate the percantege of progress = reached to goal
-    const percentage = 66;
+    const percentage = (12 / 50) * 100;
 
     return (
         <div className="mt-5">
             <div className="row d-flex justify-content-center">
                 <div className="card col-3 text-center align-items-center">
-                    <Pie percentage={20} colour="#A3BF56"/>
+                    <Pie percentage={percentage} colour="#A3BF56"/>
                     
                     <div className="d-flex justify-content-between rounded bg-light w-100 my-2">
                         <img src={RankIcon} alt="rank" className='col-2 p-2'/>
